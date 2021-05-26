42ffe6726cd323a6f5069453a43821d56845a4fc
Ettore Forigo <ettore.forigo@gmail.com>
2021-05-27 01:42:54 +0200

    dummy secret key for heroku

--- lbms/settings.py
+++ lbms/settings.py
@@ -21,8 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('secret') as f:
-    SECRET_KEY = f.read()
+
+SECRET_KEY = 'x'
+
+# LOCAL:
+# with open('secret') as f:
+#    SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
