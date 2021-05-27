272104046f828439d5e1e085050a2f697bc1b155
Ettore Forigo <ettore.forigo@gmail.com>
2021-05-27 04:17:20 +0200

    fix url issue

--- lbms/urls.py
+++ lbms/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', admin.site.urls),
     url(r'^', include('lbms_app.urls')),
+    path('', admin.site.urls),
 ]
