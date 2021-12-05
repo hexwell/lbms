31a379abd055294369b99f648cdec47ee98f3129
Ettore Forigo <ettore.forigo@gmail.com>
2021-12-05 16:56:00 +0100

    add users creation, set Users group permissions in initlbms

--- lbms_app/management/commands/initlbms.py
+++ lbms_app/management/commands/initlbms.py
@@ -2,13 +2,20 @@ from getpass import getpass
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission, Group as PermissionsGroup
+from django.contrib.contenttypes.models import ContentType
 from django.core.management import BaseCommand
 
-from lbms_app.models import Group as LbmsGroup
+from lbms_app.models import Group as LbmsGroup, Category, Source, Expense
 
 User = get_user_model()
 
 SUPERUSER_USERNAME = 'hexwell'
+
+USERS = (
+    'meri',
+    'enrico',
+    'ettore'
+)
 
 ETTORE_LBMS_GROUP_NAME = 'Ettore'
 FAMILY_LBMS_GROUP_NAME = 'Famiglia'
@@ -18,10 +25,13 @@ PERMISSION_GROUP_NAME = 'Users'
 
 def _init_lbms_groups():
     ettore_group = LbmsGroup.objects.filter(name=ETTORE_LBMS_GROUP_NAME)
+
+    if not ettore_group:
+        LbmsGroup(name=ETTORE_LBMS_GROUP_NAME).save()
+
     family_group = LbmsGroup.objects.filter(name=FAMILY_LBMS_GROUP_NAME)
 
     if not (ettore_group and family_group):
-        LbmsGroup(name=ETTORE_LBMS_GROUP_NAME).save()
         LbmsGroup(name=FAMILY_LBMS_GROUP_NAME).save()
 
 
@@ -43,6 +53,19 @@ def _init_superuser():
     user.save()
 
 
+def _init_users():
+    for u in USERS:
+        user = User.objects.filter(username=u)
+
+        if not user:
+            User.objects.create_user(
+                username=u,
+                password=getpass(f'Password for {u}: '),
+                lbms_group=LbmsGroup.objects.get(name=FAMILY_LBMS_GROUP_NAME),
+                is_staff=True
+            ).save()
+
+
 def _init_permission_group():
     group = PermissionsGroup.objects.filter(name=PERMISSION_GROUP_NAME)
 
@@ -51,6 +74,13 @@ def _init_permission_group():
 
     else:
         group = PermissionsGroup(name=PERMISSION_GROUP_NAME)
+        group.save()
+
+        for m in (Category, Source, Expense):
+            ct = ContentType.objects.get_for_model(m)
+            ps = Permission.objects.filter(content_type=ct)
+            group.permissions.add(*ps)
+
         group.save()
 
     for user in User.objects.all():
@@ -63,4 +93,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         _init_lbms_groups()
         _init_superuser()
+        _init_users()
         _init_permission_group()
